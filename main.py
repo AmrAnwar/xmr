54ae20489fbf6a89ed3f8d20e055bf444d0bd530
Amr Anwar <amranwar945@gmail.com>
2022-06-22 18:09:36 +0200

    remove bin

--- main.py
+++ main.py
@@ -13,16 +13,15 @@ def xmr():
 
 def generate(model, start_str: str):
     result = model(start_str, max_length=100)[0]['generated_text']
-    print("result:", result)
+    print("[*] result: ", result)
     return result
-
-xmr_obj = xmr()
-
 
 @app.route('/')
 def hello_world():
+    xmr_obj = xmr()
     start_str = request.args.get('start_string', default = "", type = str)
-    return generate(xmr_obj, start_str)
+    return start_str
+    # return generate(xmr_obj, start_str)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
